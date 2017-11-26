e56d7fac5b22795e7f6c3474c1f71943eec11d0d
BartlomiejWrz <bartlomiej18@op.pl>
2017-11-26 13:24:55 +0100

    readyskeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Appbar from './components/Appbar';
+import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
+import Form from './components/Form';
+import List from './components/List';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
+
+
         <div>
-            ok
+
+            <Router>
+
+                <div>
+                    <Appbar/>
+                    <Sidebar/>
+                    <Route path='/' component ={Dashboard}/>
+                    <Route path='/' component ={List}/>
+                    <Route path='/' component ={Form}/>
+                    <Footer/>
+
+                </div>
+            </Router>
         </div>
     );
   }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Sidebar extends Component{
+    render(){
+        return(
+            <div>
+                Sidebar
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
