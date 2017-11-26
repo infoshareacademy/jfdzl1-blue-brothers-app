738e526ae5b56ed6da52010958db48e5a25a6449
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-11-26 13:49:52 +0100

    Added elements to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,10 +4,22 @@ class Dashboard extends Component{
     render(){
         return(
             <div>
-                Dashboard
+                <h1>PubFinder</h1>
+                <h2>Wyszukiwarka najlepszych lokali w Twojej okolicy</h2>
+                <h3>Uruchom aplikację i wyszukaj lokal idealny dla Ciebie! </h3>
+                <button>Start</button>
             </div>
         )
     }
 }
 
 export default Dashboard;
+
+
+
+const buttonStyle = {
+    backgroundColor: 'green',
+    color: 'yellow',
+    fontSize: 20
+
+};
