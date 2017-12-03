ced61592d1ab8176937f8be72a1171f519d203ad
BartlomiejWrz <bartlomiej18@op.pl>
2017-12-03 12:12:42 +0100

    form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,9 +3,21 @@ import React, {Component} from 'react';
 class Form extends Component{
     render(){
         return(
-            <div>
-                Form
-            </div>
+            <form>
+
+                <select>
+                    <option>Bary</option>
+                    <option>Restauracje</option>
+                    <option>Fast-Food</option>
+                    <option>Dyskoteki</option>
+                </select>
+                <input type="text" placeholder="Nazwa lokalu"/>
+                <input type="text" placeholder="Adres"/>
+                <input type="text" placeholder="Strona Internetowa"/>
+                <input type="text" placeholder="Email"/>
+                <input type="text" placeholder="Telefon"/>
+                <input type="submit" value="Dodaj"/>
+            </form>
         )
     }
 }
