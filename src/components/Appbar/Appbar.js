d6bbb78bdf13692f2782be38b8a4d0c49ff8d204
Łukasz Basaj <lbasaj@gmail.com>
2017-12-03 13:19:41 +0100

    skeleton of appbar&sidebar

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
-class Appbar extends Component{
-    render(){
-        return(
-            <div>
-                Appbar
+class Appbar extends Component {
+    render() {
+        return (
+            <div className="appbar">
+                <div className="appbar-top">
+                    <p>Appbar</p>
+                    <ul className="appbar-item-wrapper">
+                        <li className="appbar-item">
+                            <Link className="appbar-link" to="/path1">Link 1</Link>
+                        </li>
+                        <li className="appbar-item">
+                            <Link className="appbar-link" to="/path2">Link 2</Link>
+                        </li>
+                        <li className="appbar-item">
+                            <Link className="appbar-link" to="/path3">Link 3</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
-class Sidebar extends Component{
-    render(){
-        return(
-            <div>
-                Sidebar
+class Sidebar extends Component {
+    render() {
+        return (
+            <div className="sidebar">
+                <Link to="/" className="sidebar-brand">
+                    {/* <img src="logo.svg" className="sidebar-logo" />*/}
+                </Link>
+
+                <div className="sidebar-left">
+                    <p>Sidebar</p>
+                    <ul className="sidebar-item-wrapper">
+                        <li className="sidebar-item">
+                            <Link className="sidebar-link" to="/path1">Link 1</Link>
+                        </li>
+                        <li className="sidebar-item">
+                            <Link className="sidebar-link" to="/path2">Link 2</Link>
+                        </li>
+                        <li className="sidebar-item">
+                            <Link className="sidebar-link" to="/path3">Link 3</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
+
         )
     }
 }
