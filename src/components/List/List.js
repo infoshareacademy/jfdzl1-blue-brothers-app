2872aa90db66dfce8a8e968302d2bd8f611f1313
Anita <anitabarann@gmail.com>
2017-12-17 11:59:58 +0100

    add list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,54 @@
 import React, {Component} from 'react';
 
-class List extends Component{
-    render(){
-        return(
+
+class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            uid: '',
+            type: '',
+            name: '',
+            weebside: '',
+            address: '',
+            locals: [{
+                uid: 'U Szewca',
+                type: 'restaurant',
+                name: 'Pub u Szewca',
+                weebside: 'http://www.uszewca.pl/',
+                address: 'Grodzka 18, 20-112 Lublin'
+            }, {
+                uid: 'Dziki Wschód',
+                type: 'bar',
+                name: 'Dziki Wschód',
+                weebside: 'http://www.uszewca.pl/',
+                address: 'Jasna 7, 20-077 Lublin'
+            }, {
+                uid: 'Trybunalska',
+                type: 'restauracja',
+                name: 'Trybunalska Lublin City Pub',
+                weebside: 'http://www.trybunalska.pl/',
+                address: 'Rynek 4, 20-001 Lublin'
+            }, {
+                uid: 'Mandragora',
+                type: 'restauracja',
+                name: 'Żydowska Restauracja Mandragoran',
+                weebside: 'http://www.mandragora.lublin.pl/en',
+                address: 'Rynek 9, 20-400 Lublin'
+            }]
+        }
+    }
+
+    render() {
+        return (
             <div>
-                List
+                {this.state.locals.map((local, index) => (
+                    <div key={index}>
+                        <h3>{uid, type, name, weebside, address}</h3>
+                        <hr/>
+                    </div>
+                ))}
             </div>
-        )
+        );
     }
 }
 
