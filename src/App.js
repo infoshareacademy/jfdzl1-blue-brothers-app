f0aea964b90483f862c7d00ce4ef7cf26192a703
Patryk Omiotek <patryk.omiotek@gmail.com>
2018-01-12 17:47:31 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import PropTypes from 'prop-types';
