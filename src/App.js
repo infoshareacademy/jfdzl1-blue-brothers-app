c23ac1760296dd78b4bc7f3f28ed395e6a6a2212
Wojciech <wojciech.paluch@o2.pl>
2018-01-28 09:27:19 +0100

    added paper to list local

--- src/App.js
+++ src/App.js
@@ -28,14 +28,14 @@ class App extends Component {
         <ConnectedRouter history={history}>
           <div>
             <div className={classes.root}>
-              <Grid container className={classes.items}>
+
                 <Appbar/>
                 <Route exact path="/" component={Dashboard}/>
                 <Route path="/list" component={LocalsList}/>
                 <Route path="/local/:localId" component={Local}/>
                 <Route path="/add" component={LocalAdd}/>
                 <Route path="/form" component={Form}/>
-              </Grid>
+
             </div>
             <Sidebar/>
             <Notifications/>
